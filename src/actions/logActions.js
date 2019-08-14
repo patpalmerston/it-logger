5a380beef25d0232a3f97555962d95a012701935
Pat Palmerston <patpalmerston@gmail.com>
2019-08-13 23:56:22 -0700

    techs reducer action and component

--- src/actions/logActions.js
+++ src/actions/logActions.js
@@ -5,9 +5,9 @@ import {
 	ADD_LOG,
 	DELETE_LOG,
 	UPDATE_LOG,
-  SEARCH_LOGS,
+	SEARCH_LOGS,
 	SET_CURRENT,
-  CLEAR_CURRENT
+	CLEAR_CURRENT
 } from './types';
 
 // export const getLogs = () => {
@@ -40,7 +40,7 @@ export const getLogs = () => async dispatch => {
 	} catch (err) {
 		dispatch({
 			type: LOGS_ERROR,
-			payload: err.response.data
+			payload: err.response.statusText
 		});
 	}
 };
@@ -66,7 +66,7 @@ export const addLog = log => async dispatch => {
 	} catch (err) {
 		dispatch({
 			type: LOGS_ERROR,
-			payload: err.response.data
+			payload: err.response.statusText
 		});
 	}
 };
@@ -87,7 +87,7 @@ export const deleteLog = id => async dispatch => {
 	} catch (err) {
 		dispatch({
 			type: LOGS_ERROR,
-			payload: err.response.data
+			payload: err.response.statusText
 		});
 	}
 };
@@ -114,7 +114,7 @@ export const updateLog = log => async dispatch => {
 	} catch (err) {
 		dispatch({
 			type: LOGS_ERROR,
-			payload: err.response.data
+			payload: err.response.statusText
 		});
 	}
 };
@@ -134,7 +134,7 @@ export const searchLogs = text => async dispatch => {
 	} catch (err) {
 		dispatch({
 			type: LOGS_ERROR,
-			payload: err.response.data
+			payload: err.response.statusText
 		});
 	}
 };
