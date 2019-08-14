01c699aeba68b3d67248da82f8009f43d922d6ce
Pat Palmerston <patpalmerston@gmail.com>
2019-08-13 22:55:57 -0700

    delete logs

--- src/components/logs/LogItem.js
+++ src/components/logs/LogItem.js
@@ -1,32 +1,48 @@
 import React from 'react';
-import Moment from 'react-moment'
+import Moment from 'react-moment';
+import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
+import { deleteLog } from '../../actions/logActions';
 
-const LogItem = ({ log }) => {
+import M from 'materialize-css/dist/js/materialize.min.js';
+
+const LogItem = ({ log, deleteLog }) => {
+	const onDelete = () => {
+		deleteLog(log.id);
+		M.toast({ html: 'Log Deleted' });
+	};
+
 	return (
 		<li className='collection-item'>
 			<div>
-        <a 
-          href='#edit-log-modal' 
-          className={`modal-trigger ${log.attention ? 'red-text' : 'blue-text'}`}
-          >
-          {log.message}
-          </a>
-          <br />
-          <span className='grey-text'>
-            <span className="black-text">ID #{log.id}</span> last updated by {' '}
-            <span className="black-text">{log.tech}</span> on <Moment format='MMM Do YYY, h: mm: ss a'>{log.date}</Moment>
-          </span>
-          <a href="#!" className="secondary-content">
-            <i className="material-icons grey-text">delete</i>
-          </a>
+				<a
+					href='#edit-log-modal'
+					className={`modal-trigger ${
+						log.attention ? 'red-text' : 'blue-text'
+					}`}
+				>
+					{log.message}
+				</a>
+				<br />
+				<span className='grey-text'>
+					<span className='black-text'>ID #{log.id}</span> last updated by{' '}
+					<span className='black-text'>{log.tech}</span> on{' '}
+					<Moment format='MMM Do YYY, h: mm: ss a'>{log.date}</Moment>
+				</span>
+				<a href='#!' onClick={onDelete} className='secondary-content'>
+					<i className='material-icons grey-text'>delete</i>
+				</a>
 			</div>
 		</li>
 	);
 };
 
 LogItem.propTypes = {
-	log: PropTypes.object.isRequired
+	log: PropTypes.object.isRequired,
+	deleteLog: PropTypes.func.isRequired
 };
 
-export default LogItem;
+export default connect(
+	null,
+	{ deleteLog }
+)(LogItem);
