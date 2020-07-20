336547cd23b6b096d06d20d9534beab854c534ab
Pat Palmerston <patpalmerston@gmail.com>
2020-07-20 09:35:41 -0700

    Updates

---
+++ precache-manifest.5a6152fbef1ae92315440f229abb86dc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e0ab23a03786802df8731ad2ed7e6a70",
+    "url": "/index.html"
+  },
+  {
+    "revision": "04fc9ad6286cfff6cd12",
+    "url": "/static/css/2.ae9e8b99.chunk.css"
+  },
+  {
+    "revision": "dc009ba40a4d152faba9",
+    "url": "/static/css/main.36497136.chunk.css"
+  },
+  {
+    "revision": "04fc9ad6286cfff6cd12",
+    "url": "/static/js/2.681f68f6.chunk.js"
+  },
+  {
+    "revision": "dc009ba40a4d152faba9",
+    "url": "/static/js/main.c8899534.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
